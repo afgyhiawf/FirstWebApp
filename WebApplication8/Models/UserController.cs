3b0f94e7ed72a24a0f7190263473259ea69c5df9
MaxPanenko <panenko123456789987654321@gmail.com>
2023-04-25 19:34:49 +0200

    Add project files.

---
+++ WebApplication8/Models/UserController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication8.Models
+{
+    public class UserController
+    {
+        public int Age { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
